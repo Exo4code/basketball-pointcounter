f1a7f1bec83a2166ca7d8aa00edef3b829659bc7
Exo4code <95982749+Exo4code@users.noreply.github.com>
2025-03-05 22:38:50 +0100

    added complete files

---
+++ index.js
@@ -0,0 +1,65 @@
+
+
+
+let homeOne = document.getElementById("home-score") 
+count = 0
+
+
+function incrementOne() {
+    count +=  1
+    homeOne.innerText = count
+}
+
+let homeTwo = document.getElementById("home-score") 
+
+
+
+function incrementTwo() {
+    count  += 2
+    homeTwo.innerText = count
+}
+
+
+let homeThree = document.getElementById("home-score") 
+
+
+function incrementThree() {
+    count += 3
+    homeThree.innerText = count
+}
+
+
+
+/***************/
+
+
+
+
+let guestOne = document.getElementById("guest-score") 
+guestcount = 0
+
+
+function incrementOneGuest() {
+    guestcount +=  1
+    guestOne.innerText = guestcount
+}
+
+let guestTwo = document.getElementById("guest-score") 
+
+
+
+function incrementTwoGuest() {
+    guestcount  += 2
+    guestTwo.innerText = guestcount
+}
+
+
+let guestThree = document.getElementById("guest-score") 
+
+
+function incrementThreeGuest() {
+    guestcount += 3
+    guestThree.innerText = guestcount
+}
+
+
